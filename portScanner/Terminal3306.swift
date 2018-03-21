56700d8d35d15bfefbf7da07841b7d1845c45721
Fernando Ortiz Rico Celio <fernando_oc96@hotmail.com>
2018-03-21 16:39:24 -0600

    New terminal view added to the project.

---
+++ portScanner/Terminal3306.swift
@@ -0,0 +1,37 @@
+//
+//  Terminal3306.swift
+//  portScanner
+//
+//  Created by Fernando Ortiz Rico Celio on 3/21/18.
+//  Copyright © 2018 Fernando Ortiz Rico Celio. All rights reserved.
+//
+
+import UIKit
+
+class Terminal3306: UIViewController {
+    //Elementos
+    @IBOutlet weak var consoleText: UITextView!
+    @IBOutlet weak var mensajeText: TextViewDesign!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func enviarMensaje(_ sender: Any) {
+        //Botón enviar presionado
+        
+    }
+    
+    
+
+}
